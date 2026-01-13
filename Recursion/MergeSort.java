bb3441d11c1310a02d68bee5a9a38b52f9ed7cc7
Severus <16ajeetkumar@gmail.com>
2026-01-14 01:39:27 +0530

    MergeSort done

--- Recursion/MergeSort.java
+++ Recursion/MergeSort.java
@@ -2,6 +2,52 @@ import java.util.*;
 
 class mergeSort{
     public static void main(String[] args){
-        System.out.println("hello");
+        int[] arr = {93, 2, 19, 6, 81 , 5};
+        arr = mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static int[] mergeSort(int[] arr){
+        if(arr.length == 1){
+            return arr;
+        }
+
+        //divide in two
+        int mid = arr.length/2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left,right);
+    }
+
+    private static int[] merge(int[] left, int[] right){
+        int i = 0, j = 0, k = 0;
+        int[] mix = new int[left.length + right.length];
+
+        while(i < left.length && j < right.length){
+            if(left[i] < right[j]){
+                mix[k] = left[i];
+                i++;
+            }
+            else{
+                mix[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < left.length){
+            mix[k] = left[i];
+            i++;
+            k++;
+        }
+        while(j < right.length){
+            mix[k] = right[j];
+            j++;
+        }
+
+        return mix;
     }
 }
